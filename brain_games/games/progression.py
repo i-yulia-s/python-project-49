ccaaee586690762740becc958ba75d085300361e
i-yulia-s <i-yulia@mail.ru>
2025-03-07 00:42:52 +0400

    Add game 'Progression'

---
+++ brain_games/games/progression.py
@@ -0,0 +1,15 @@
+from random import randrange, choice
+from brain_games.engine import play_game
+
+
+def guess_progression():
+    input = []
+    for i in range(0, 3):
+        progression = [randrange(1, 50)]
+        difference = randrange(2, 10)
+        for j in range(1, 10):
+            progression.append(progression[j - 1] + difference)
+        answer_index = randrange(0, 9)
+        answer, progression[answer_index] = progression[answer_index], '..'
+        input.append((' '.join(str(i) for i in progression), str(answer)))
+    return play_game(input)
