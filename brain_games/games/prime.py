554fc2e01ac18879a7b1c9183b3b441e5cf5c7fd
i-yulia-s <i-yulia@mail.ru>
2025-03-08 13:53:07 +0400

    Add 'Prime' game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,26 @@
+from random import randrange
+from brain_games.engine import play_game
+
+
+def is_prime(num):
+    if num <= 1 or num % 2 == 0 or num % 3 == 0:
+        return False
+    if num == 2 or num == 3:
+        return True
+    i = 5
+    d = 2
+    while i ** 2 <= num:
+        if num % i == 0:
+            return False
+        i += d
+        d = 6 - d
+    return True
+
+
+def guess_prime():
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    input = []
+    for i in range(0, 3):
+        question = randrange(1, 100)
+        input.append((question, 'yes' if is_prime(question) else 'no'))
+    return play_game(input)  
