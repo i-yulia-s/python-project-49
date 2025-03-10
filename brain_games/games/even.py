6f30173b584eb55cdf588ba5d9df7157b21cb7f7
i-yulia-s <i-yulia@mail.ru>
2025-03-11 01:22:20 +0400

    Separate function for checking if the number is even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,10 +3,17 @@ from random import randrange
 from brain_games.engine import play_game
 
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False    
+
+
 def guess_even():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     input = []
     for i in range(0, 3):
         question = randrange(20)
-        input.append((question, 'yes' if question % 2 == 0 else 'no'))
+        input.append((question, 'yes' if is_even(question) else 'no'))
     return play_game(input)
