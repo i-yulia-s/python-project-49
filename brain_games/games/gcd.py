02f9c7e02cc5be63af5f1f87ab00887652502c4d
i-yulia-s <i-yulia@mail.ru>
2025-03-11 01:36:02 +0400

    Separate function for calculating GCD

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,15 +3,17 @@ from random import randrange
 from brain_games.engine import play_game
 
 
+def gcd(number1, number2):
+    while number1 % number2 != 0:
+        number1, number2 = number2, number1 % number2
+    return number2
+
+
 def guess_gcd():
     print('Find the greatest common divisor of given numbers.')
     input = []
     for i in range(0, 3):
         num1 = randrange(1, 100)
         num2 = randrange(1, 100)
-        i = num1
-        j = num2
-        while i % j != 0:
-            i, j = j, i % j
-        input.append((f'{num1} {num2}', str(j)))
+        input.append((f'{num1} {num2}', str(gcd(num1, num2))))
     return play_game(input)
