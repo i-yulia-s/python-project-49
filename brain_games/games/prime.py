e60c4854c0e0724de2b8d6fda55e9b44540c5956
i-yulia-s <i-yulia@mail.ru>
2025-03-11 01:32:13 +0400

    Fix logic in "Prime"

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,15 @@ from brain_games.engine import play_game
 
 
 def is_prime(num):
-    # 1 is not a prime
-    # All numbers divisible by 2 and 3 are not primes
-    if num <= 1 or num % 2 == 0 or num % 3 == 0:
-        return False
     # 2 and 3 are the only primes divisible by 2 or 3.
     # We make sure that our number is not 2 or 3,
     # and then skip divisors 2 and 3 in the check.
     if num == 2 or num == 3:
         return True
+    # 1 is not a prime
+    # All numbers divisible by 2 and 3 are not primes
+    if num <= 1 or num % 2 == 0 or num % 3 == 0:
+        return False
     # If the number is divisible by 2*n, it's also divisible by 2.
     # Therefore, we skip 2*n.
     # Check starts from 5 (4 excluded):
