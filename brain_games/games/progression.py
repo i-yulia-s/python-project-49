ff348633fd54c17b9b2b28930d75787ad79d0536
i-yulia-s <i-yulia@mail.ru>
2025-03-11 02:06:06 +0400

    Make separate functions to generate progression and replace a number by dots

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,15 +3,24 @@ from random import randrange
 from brain_games.engine import play_game
 
 
+def create_random_progression():
+    progression = [randrange(1, 50)]
+    difference = randrange(2, 10)
+    for j in range(1, 10):
+        progression.append(progression[j - 1] + difference)
+    return progression
+
+
+def get_number_and_replace_by_dots(progression, index):
+    answer, progression[index] = progression[index], '..'
+    return answer
+
+
 def guess_progression():
     print('What number is missing in the progression?')
     input = []
     for i in range(0, 3):
-        progression = [randrange(1, 50)]
-        difference = randrange(2, 10)
-        for j in range(1, 10):
-            progression.append(progression[j - 1] + difference)
-        answer_index = randrange(0, 9)
-        answer, progression[answer_index] = progression[answer_index], '..'
-        input.append((' '.join(str(i) for i in progression), str(answer)))
+        question = create_random_progression()
+        answer = get_number_and_replace_by_dots(question, randrange(0, 9))    
+        input.append((' '.join(str(j) for j in question), str(answer)))
     return play_game(input)
