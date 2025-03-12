7c2e4cd88528f2af6c80b4f5256991a29e437a9e
i-yulia-s <i-yulia@mail.ru>
2025-03-12 21:09:32 +0400

    Separate function for creating and calculating expression

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,18 +1,20 @@
 from operator import add, mul, sub
 from random import choice, randrange
-
 from brain_games.engine import play_game
 
+
+def get_random_operation(n1, n2):
+    op_map = {"+": add,
+        "-": sub,
+        "*": mul}
+    operator = choice(["+", "-", "*"])
+    return (f'{str(n1)} {operator} {str(n2)}', str(op_map[operator](n1, n2)))
 
 def guess_calc():
     print('What is the result of the expression?')
     input = []
-    op_glos = {"+": add,
-               "-": sub,
-               "*": mul}
     for i in range(0, 3):
         n1 = randrange(20)
         n2 = randrange(20)
-        op = choice(["+", "-", "*"])
-        input.append((f'{str(n1)} {op} {str(n2)}', str(op_glos[op](n1, n2))))
+        input.append(get_random_operation(n1, n2))
     return play_game(input)
